c13664de2598530ed672f40cfad2155a61e7c31f
Cristian Garcia <crisg@sas.upenn.edu>
2023-12-08 14:55:18 -0500

    added state for the book genre and the book recommendations; added the genre and the recommendations as a list

--- src/components/BookRecommendations.jsx
+++ src/components/BookRecommendations.jsx
@@ -6,21 +6,31 @@ import bookData from "../books.json";
 const BookRecommendations = () => {
   // State for all book data
   const [books, setBooks] = useState(bookData);
+  const genres = Object.keys(books);
 
   // SUGGESTED TODOS:
   // TODO: Implement state for selected genre and recommendations
+  const [genre, setGenre] = useState("");
   // TODO: Implement state for recommendations
+  const [recommendations, setRecommendations] = useState([]);
   // TODO: Implement the handleGenreSelect function
+  function handleGenreClick (genre) {
+    setGenre(genre);
+    // set the book recommendations based on this genre
+    const recommendedBooks = books[genre];
+    setRecommendations(recommendedBooks);
+  }
 
   return (
     <div className="book-recommendation-engine">
       <h2>Book Recommendation Engine</h2>
       <div className="genre-buttons">
-        {/* TODO: Map over genres and create buttons */}
+        {genres.map(genre => <button onClick={e => handleGenreClick(e.target.textContent)} key={genre}>{genre}</button>)}
       </div>
       <div>
         <h3>Recommendations:</h3>
-        {/* TODO: Display recommendations based on selected genre */}
+        <h2>Recommendations for {genre}</h2>
+        {recommendations.map(book => <li key={book}>{book}</li>)}
       </div>
     </div>
   );
