0d41a2ccafb34db3aa0591baf9c39f4ce8552343
juligarc91 <julissagarcia@pursuit.org>
2023-12-08 19:28:45 -0500

    created .map fx

--- src/components/BookRecommendations.jsx
+++ src/components/BookRecommendations.jsx
@@ -11,16 +11,24 @@ const BookRecommendations = () => {
   // TODO: Implement state for selected genre and recommendations
   // TODO: Implement state for recommendations
   // TODO: Implement the handleGenreSelect function
+  const [genres, setGenre] = useState(Object.keys(books));
+  //console.log (genres);
+  const [recommendations, setRecommendations] = useState(Object.values(books));
+  //console.log (recommendations)
 
   return (
     <div className="book-recommendation-engine">
       <h2>Book Recommendation Engine</h2>
       <div className="genre-buttons">
-        {/* TODO: Map over genres and create buttons */}
+        <button>
+        {genres.map(genre => genre)}
+        </button>
       </div>
       <div>
         <h3>Recommendations:</h3>
-        {/* TODO: Display recommendations based on selected genre */}
+        <button>
+        {recommendations.map(recommendation => recommendation)}
+        </button>
       </div>
     </div>
   );
