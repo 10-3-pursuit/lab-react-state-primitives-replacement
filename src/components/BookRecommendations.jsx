0381c2f80e5a0e426eb115c2fad5540a9f8d9651
Jennifer <jenniferpeterson@pursuit.org>
2023-12-09 10:00:00 -0500

    react state primitive replacement lab complete 👩🏿‍💻

--- src/components/BookRecommendations.jsx
+++ src/components/BookRecommendations.jsx
@@ -1,26 +1,47 @@
-// BookRecommendationEngine.jsx
 import React, { useState } from "react";
 import "./BookRecommendations.css";
 import bookData from "../books.json";
 
 const BookRecommendations = () => {
-  // State for all book data
+  // State to hold the book data
   const [books, setBooks] = useState(bookData);
 
-  // SUGGESTED TODOS:
-  // TODO: Implement state for selected genre and recommendations
-  // TODO: Implement state for recommendations
-  // TODO: Implement the handleGenreSelect function
+  // State to keep track of the selected genre
+  const [selectedGenre, setSelectedGenre] = useState(null);
+
+  // Function to handle selecting a genre
+  const handleGenreSelect = (genre) => {
+    // Toggle selected genre: If the same genre is clicked again, it resets the selectedGenre state to null
+    setSelectedGenre(selectedGenre === genre ? null : genre);
+  };
 
   return (
-    <div className="book-recommendation-engine">
+    <div className="book-recommendations book-recommendation-engine">
       <h2>Book Recommendation Engine</h2>
       <div className="genre-buttons">
-        {/* TODO: Map over genres and create buttons */}
+        {/* Map over genres and create buttons */}
+        {Object.keys(books).map((genre) => (
+          // Create a button for each genre
+          <button
+            className="genre-buttons"
+            key={genre}
+            onClick={() => handleGenreSelect(genre)}
+          >
+            {genre}
+          </button>
+        ))}
       </div>
       <div>
         <h3>Recommendations:</h3>
-        {/* TODO: Display recommendations based on selected genre */}
+        {/* Display recommendations based on selected genre */}
+        {selectedGenre && ( // If a genre is selected, display recommendations
+          <ul className="book-recommendations book-list">
+            {/* Map over the books in the selected genre and create a list */}
+            {books[selectedGenre].map((book, index) => (
+              <li key={index}>{book}</li>
+            ))}
+          </ul>
+        )}
       </div>
     </div>
   );
