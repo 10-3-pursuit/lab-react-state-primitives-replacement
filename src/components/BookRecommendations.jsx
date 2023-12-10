b2b931d1309ec5cc689084f455070bebc5625762
juligarc91 <julissagarcia@pursuit.org>
2023-12-09 22:30:08 -0500

    Made h3 dynamic

--- src/components/BookRecommendations.jsx
+++ src/components/BookRecommendations.jsx
@@ -3,35 +3,109 @@ import React, { useState } from "react";
 import "./BookRecommendations.css";
 import bookData from "../books.json";
 
+// const genreString = Object.keys(bookData).find (genre => { 
+//   //add if statement to choose between genres and make it match button click for example if clicking fantasy genre would = fantasy and not all the strings
+//   genre === onClick={() => handleGenreSelect(genre)
+// }
+// );
+// console.log (genreString);
+
+let genreString = "";
+
 const BookRecommendations = () => {
   // State for all book data
   const [books, setBooks] = useState(bookData);
+  // const [genres, setGenre] = useState(Object.keys(books)); // array of the keys
+  // const [recommendations, setRecommendations] = useState(Object.values(books)); // array of the vaues
 
-  // SUGGESTED TODOS:
-  // TODO: Implement state for selected genre and recommendations
-  // TODO: Implement state for recommendations
-  // TODO: Implement the handleGenreSelect function
-  const [genres, setGenre] = useState(Object.keys(books));
-  //console.log (genres);
-  const [recommendations, setRecommendations] = useState(Object.values(books));
-  //console.log (recommendations)
+  const [ourGenre, setOurGenre] = useState(null);
+  const [recommendations, setRecommendations] = useState([]);
+
+
+  
+  const handleGenreSelect = (genre) => {
+    // const chosenGenre = Object.keys(books).filter ()
+    const chosenGenre = bookData[genre] || [];
+    
+    // maybe include: if statement or ternary or switch to toggle between use state
+    
+    setRecommendations(chosenGenre);
+    setOurGenre(genre);
+    // console.log (chosenGenre);
+  };
 
   return (
-    <div className="book-recommendation-engine">
+    <div className="book-recommendations">
       <h2>Book Recommendation Engine</h2>
       <div className="genre-buttons">
-        <button>
-        {genres.map(genre => genre)}
-        </button>
+
+        {/* add back ticks to add 2 classes */}
+        {Object.keys(bookData).map((genre)=> <button key={genre} className={`${ourGenre === genre ? "active" : ""} `} 
+        onClick={() =>{
+         handleGenreSelect(genre)
+         genreString = genre}
+         
+         }>
+          {genre}
+          </button>)}
       </div>
       <div>
-        <h3>Recommendations:</h3>
-        <button>
-        {recommendations.map(recommendation => recommendation)}
-        </button>
+        <h3>Recommendations for {genreString}:</h3>
+        {recommendations.length > 0 ? (
+          <ul className="book-list">
+            {recommendations.map((bookTitle, index) => (
+              <li key={index} >{bookTitle}</li>
+            ))}
+          </ul>
+        ):(<p> No recommendations available.</p>
+        )}
+        </div>
       </div>
-    </div>
   );
 };
 
 export default BookRecommendations;
+
+/* ----- NOTES React and JavaScript best practices -----
+
+Directly assigning a value to a variable (`genreString = genre`) inside an `onClick` handler within JSX is not a typical or recommended approach in React. It might work, but it's not the standard way to handle state or variable assignments in React components. Usually, state management in React is done using the `useState` hook or other state management solutions.
+
+If `genreString` is meant to be a state that reflects the currently selected genre, it should be handled using the `useState` hook, and updated via a function like `setGenreString`. Here's an example of how to do this:
+
+```jsx
+import React, { useState } from 'react';
+
+function MyComponent({ bookData, ourGenre, handleGenreSelect }) {
+  const [genreString, setGenreString] = useState('');
+
+  const handleClick = (genre) => {
+    handleGenreSelect(genre);
+    setGenreString(genre);
+  };
+
+  return (
+    <div>
+      {Object.keys(bookData).map((genre) => (
+        <button
+          key={genre}
+          className={ourGenre === genre ? "active" : ""}
+          onClick={() => handleClick(genre)}
+        >
+          {genre}
+        </button>
+      ))}
+    </div>
+  );
+}
+
+export default MyComponent;
+```
+
+In this revised code:
+
+- `genreString` is a state variable initialized with `useState`.
+- `setGenreString` is used to update `genreString` when a genre button is clicked.
+- `handleClick` is a new function that encapsulates both the `handleGenreSelect` call and the state update for `genreString`.
+
+This approach aligns with React's principles of state management and makes your component's behavior more predictable and maintainable.
+*/
