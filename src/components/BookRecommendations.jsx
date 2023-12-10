b9d432619ca3da52db43c827c638f5887dc1b365
juligarc91 <julissagarcia@pursuit.org>
2023-12-10 18:59:25 -0500

    Elaborated on use of useState and used it on h3 element

--- src/components/BookRecommendations.jsx
+++ src/components/BookRecommendations.jsx
@@ -3,60 +3,69 @@ import React, { useState } from "react";
 import "./BookRecommendations.css";
 import bookData from "../books.json";
 
-let genreString = "";
+// let genreString = "";
 
+// Define a functional component named BookRecommendations to create an interface for book recommendations.
 const BookRecommendations = () => {
-  // State for all book data
+  // Initialize a state variable 'books' with the data from 'bookData', enabling dynamic management of book data within the component.
   const [books, setBooks] = useState(bookData);
-  // const [genres, setGenre] = useState(Object.keys(books)); // array of the keys
-  // const [recommendations, setRecommendations] = useState(Object.values(books)); // array of the vaues
 
-  const [ourGenre, setOurGenre] = useState(null);
+  // Commented out lines for reference - these were initial approaches for handling genre and recommendation data separately.
+  // const [genres, setGenre] = useState(Object.keys(books)); // Creates a list of genres from the book data keys for potential filtering or display purposes.
+  // const [recommendations, setRecommendations] = useState(Object.values(books)); // Creates a list of book recommendations from the book data values, potentially for display.
+
+  // Establish a state to keep track of the currently selected genre, allowing the component to react and update the display based on user selection.
+  const [ourGenre, setOurGenre] = useState("");
+  // Initialize an empty array for recommendations, to be updated based on the selected genre, ensuring dynamic content presentation.
   const [recommendations, setRecommendations] = useState([]);
 
-
-  
+  // Define a function to handle changes in genre selection, crucial for updating the displayed recommendations based on user interaction.
   const handleGenreSelect = (genre) => {
-    // const chosenGenre = Object.keys(books).filter ()
-    const chosenGenre = bookData[genre] || [];
+    // Retrieve the list of books for the selected genre, defaulting to an empty array if the genre isn't in the data, thus ensuring robustness.
+    const genreArr = bookData[genre] || []
     
-    // maybe include: if statement or ternary or switch to toggle between use state
-    
-    setRecommendations(chosenGenre);
+    // Update the recommendations state with the books from the selected genre, dynamically altering the displayed recommendations.
+    setRecommendations(genreArr);
+    // Update the ourGenre state with the selected genre, which is used to display the current genre to the user.
     setOurGenre(genre);
-    // console.log (chosenGenre);
+    // Commented out line - an initial approach to filtering genres, kept for reference or potential future use.
+    // const chosenGenre = Object.keys(books).filter ()
   };
 
+  // The return statement provides the JSX layout of the component, defining its structure and content on the webpage.
   return (
     <div className="book-recommendations">
       <h2>Book Recommendation Engine</h2>
       <div className="genre-buttons">
-
-        {/* add back ticks to add 2 classes */}
-        {Object.keys(bookData).map((genre)=> <button key={genre} 
-        onClick={() =>{
-         handleGenreSelect(genre)
-         genreString = genre}
-         }>
-          {genre}
-          </button>)}
+        {/* Iterate over each genre in bookData to create a button for it, enabling user interaction for genre selection. */}
+        {Object.keys(bookData).map((genre) => (
+          <button key={genre} 
+                  onClick={() => handleGenreSelect(genre)}>
+            {genre}
+          </button>
+        ))}
       </div>
       <div>
-        <h3>Recommendations for {genreString}:</h3>
+        <h3>Recommendations for {ourGenre}:</h3>
+        {/* Conditional rendering to display recommendations if available, or a prompt to select a genre otherwise, enhancing user experience. */}
         {recommendations.length > 0 ? (
           <ul className="book-list">
+            {/* Map each book title in the recommendations to a list item, dynamically creating a list of books based on the selected genre. */}
             {recommendations.map((bookTitle, index) => (
-              <li key={index} >{bookTitle}</li>
+              <li key={index}>{bookTitle}</li>
             ))}
           </ul>
-        ):(<p> No recommendations available.</p>
+        ) : (
+          <p>Please select a genre to view Book Recommendations</p>
         )}
-        </div>
       </div>
+    </div>
   );
 };
 
+// Export the BookRecommendations component to enable its use in other parts of the application, facilitating modularity and reuse.
 export default BookRecommendations;
+
 // --- CSS Notes ---
 /*
 You can toggle between styles using ternary in class names
