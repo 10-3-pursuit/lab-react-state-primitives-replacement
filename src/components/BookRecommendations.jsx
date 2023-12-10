fad2dbaa2774a096549c4b6fcb2fe7dfc5b1879f
MichaelARestrepoross <michaelrestrepoross@pursuit.org>
2023-12-10 17:55:46 -0500

    Updated BookRecommendations.jsx

--- src/components/BookRecommendations.jsx
+++ src/components/BookRecommendations.jsx
@@ -9,19 +9,53 @@ const BookRecommendations = () => {
 
   // SUGGESTED TODOS:
   // TODO: Implement state for selected genre and recommendations
+  const [selectedGenre, setSelectedGenre] = useState('');
   // TODO: Implement state for recommendations
+  const [recommendations, setRecommendations] = useState([]);
+
+  const [showAllBooks, setShowAllBooks] = useState(false);
+  
   // TODO: Implement the handleGenreSelect function
+  const handleGenreSelect = (selectedGenre) => {
+    setSelectedGenre(selectedGenre);
+    setRecommendations(bookData[selectedGenre].slice(0, 2) || []); 
+    setShowAllBooks(false);
+  };
+  
+  const handleMoreLikeThis = () => {
+    const genreBooks = bookData[selectedGenre] || [];
+    setRecommendations(genreBooks);
+    setShowAllBooks(true);
+  };
 
   return (
-    <div className="book-recommendation-engine">
+    <div className="book-recommendations ">
       <h2>Book Recommendation Engine</h2>
       <div className="genre-buttons">
         {/* TODO: Map over genres and create buttons */}
+        {Object.keys(bookData).map((genre, index) => (
+          <button key={genre} onClick={() => handleGenreSelect(genre)}>
+            {genre}
+          </button>
+        ))}
       </div>
+
+      
+      {selectedGenre && ( 
       <div>
-        <h3>Recommendations:</h3>
+        <h3>Recommendations for {selectedGenre}:</h3>
         {/* TODO: Display recommendations based on selected genre */}
+        <ul className="book-list">
+          {recommendations.map((bookTitle, index) => (
+            <li key={index}>
+              {bookTitle}
+            </li>
+          ))}
+        </ul>
+        <button onClick={handleMoreLikeThis}>More Like This</button>
       </div>
+      )}
+
     </div>
   );
 };
