3d273bcd19e3877b32b04bcf34b866014a724342
MarlonPelau <MarlonRegis@pursuit.org>
2023-12-11 00:52:52 -0500

    late night sessionwith Enoch, Anita and Luis for 3hrs

--- src/components/BookRecommendations.jsx
+++ src/components/BookRecommendations.jsx
@@ -6,21 +6,44 @@ import bookData from "../books.json";
 const BookRecommendations = () => {
   // State for all book data
   const [books, setBooks] = useState(bookData);
-
+  const [newGenre, setNewGenre] = useState("");
+  const [nuRecommendations, setNuRecommendations] = useState([]);
   // SUGGESTED TODOS:
   // TODO: Implement state for selected genre and recommendations
   // TODO: Implement state for recommendations
   // TODO: Implement the handleGenreSelect function
+  
+  const handleGenreSelect = (genre) => {
+    const bookSelection = books[genre];
+    setNuRecommendations(bookSelection);
+    setNewGenre(genre);
+  };
 
   return (
-    <div className="book-recommendation-engine">
+    <div className="book-recommendations">
       <h2>Book Recommendation Engine</h2>
       <div className="genre-buttons">
+       {Object.keys(books).map((genre) => (
+        <button
+          key = {genre}
+              className = {newGenre === genre && "active"}
+              onClick = {() => {handleGenreSelect(genre)}}
+        > {genre}
+        </button>))}
+        
         {/* TODO: Map over genres and create buttons */}
       </div>
-      <div>
-        <h3>Recommendations:</h3>
+      <div >
+        <h3>Recommendations for {newGenre}: </h3>
         {/* TODO: Display recommendations based on selected genre */}
+        {nuRecommendations.length > 0 ? (
+          <ul className="book-list" >
+            {nuRecommendations.map((bookTitle, index) => (
+              <li key={index}>{bookTitle}</li>
+            ))}
+          </ul>
+        ):( <p>choose a genre.</p>
+        )}
       </div>
     </div>
   );
