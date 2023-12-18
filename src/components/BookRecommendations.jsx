a86c63f5a2c4f219879bf33a1a45749d87f680c3
Jazon Younge <jay@Jazons-Air.lan>
2023-12-18 16:50:38 -0500

    lab completed bare minimum.

--- src/components/BookRecommendations.jsx
+++ src/components/BookRecommendations.jsx
@@ -6,21 +6,42 @@ import bookData from "../books.json";
 const BookRecommendations = () => {
   // State for all book data
   const [books, setBooks] = useState(bookData);
-
+  const [displayBook, setDisplayBook] = useState([]);
   // SUGGESTED TODOS:
   // TODO: Implement state for selected genre and recommendations
+  const [selectedGenre, setSelectedGenre] = useState("");
   // TODO: Implement state for recommendations
+  const [recommendations, setRecommendations] = useState([]);
   // TODO: Implement the handleGenreSelect function
-
+  const handleGenreSelect = (genre) => {
+    setSelectedGenre(genre);
+    setRecommendations(books[genre]);
+  };
   return (
-    <div className="book-recommendation-engine">
+    <div className="book-recommendations">
       <h2>Book Recommendation Engine</h2>
       <div className="genre-buttons">
         {/* TODO: Map over genres and create buttons */}
+        {Object.keys(bookData).map((genre) => (
+          <button
+            key={genre}
+            onClick={() => {
+              console.log(`Genre is ${genre}.`);
+              handleGenreSelect(genre);
+            }}
+          >
+            {genre}
+          </button>
+        ))}
       </div>
       <div>
         <h3>Recommendations:</h3>
         {/* TODO: Display recommendations based on selected genre */}
+        <ul className="book-list">
+          {recommendations.map((book) => (
+            <li key={book}>{book}</li>
+          ))}
+        </ul>
       </div>
     </div>
   );
