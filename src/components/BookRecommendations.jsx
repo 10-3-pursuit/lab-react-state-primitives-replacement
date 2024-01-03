8f90b1629390977d52c83c9377d97d20c10adaf9
jadenglenn <jadenglenn@pursuit.org>
2024-01-03 16:10:14 -0500

    Engine working

--- src/components/BookRecommendations.jsx
+++ src/components/BookRecommendations.jsx
@@ -4,23 +4,51 @@ import "./BookRecommendations.css";
 import bookData from "../books.json";
 
 const BookRecommendations = () => {
+
   // State for all book data
   const [books, setBooks] = useState(bookData);
+
+  const [selectedGenre, setSelectedGenre] = useState(null)
+
+
+  const handleGenre = (genre) => {
+    
+    setSelectedGenre(genre)
+  }
+
+  const getReccomendations = () => {
+    if (selectedGenre) {
+      return (
+        <div>
+        <ol>
+          {books[selectedGenre].map((book, index) => (
+            <li key={index}>{book}</li>
+          ))}
+        </ol>
+        </div>
+      );
+    } else {
+      return <p>Select a genre</p>
+    }
+  }
 
   // SUGGESTED TODOS:
   // TODO: Implement state for selected genre and recommendations
   // TODO: Implement state for recommendations
   // TODO: Implement the handleGenreSelect function
-
+  
   return (
     <div className="book-recommendation-engine">
       <h2>Book Recommendation Engine</h2>
       <div className="genre-buttons">
-        {/* TODO: Map over genres and create buttons */}
+        {Object.keys(books).map((genre) => (
+          <button key={genre} onClick= {() => handleGenre(genre)}>{genre}</button>
+        ))}
+       
       </div>
       <div>
         <h3>Recommendations:</h3>
-        {/* TODO: Display recommendations based on selected genre */}
+        {getReccomendations()}
       </div>
     </div>
   );
